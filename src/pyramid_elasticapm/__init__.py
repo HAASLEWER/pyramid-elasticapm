ed600223a26c579a9032ccf2fbe22d101c848e44
HAASLEWER <haaslewer2@gmail.com>
2021-06-30 12:12:13 +0200

    Use pyramid 1.10.4

--- src/pyramid_elasticapm/__init__.py
+++ src/pyramid_elasticapm/__init__.py
@@ -6,7 +6,18 @@ import elasticapm
 import pkg_resources
 from elasticapm.utils import compat, get_url_dict
 from pyramid.events import ApplicationCreated, subscriber
-from pyramid.util import reraise
+
+
+def reraise(tp, value, tb=None):
+    try:
+        if value is None:
+            value = tp()
+        if value.__traceback__ is not tb:
+            raise value.with_traceback(tb)
+        raise value
+    finally:
+        value = None
+        tb = None
 
 
 def includeme(config):
